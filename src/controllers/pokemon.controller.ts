77cfbecf22c6b49a4f26908f1da2e211b4421e76
Coderman47 <askinglucho@gmail.com>
2025-05-29 17:35:22 -0300

    add if conditional to check the existing pokemons

--- src/controllers/pokemon.controller.ts
+++ src/controllers/pokemon.controller.ts
@@ -13,9 +13,12 @@ export class PokemonController {
   async getAllPokemon() {
     try {
       const pokemons = await this.pokemonRepository.find();
+      if (pokemons.length === 0) {
+        throw new Error('No pokemons found');
+      }
       return pokemons;
     } catch (error) {
-      throw new Error('Error fetching pokemons');
+      throw new Error('Error fetching pokemons: ', error);
     }
   }
 }
